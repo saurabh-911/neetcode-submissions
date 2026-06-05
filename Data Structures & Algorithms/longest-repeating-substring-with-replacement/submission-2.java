4e12907667c227bda40ed55ea62a2a4c2c963937
saurabh-911 <165826656+saurabh-911@users.noreply.github.com>
2026-06-05 16:59:40 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -0,0 +1,64 @@
+// class Solution {
+//     public int characterReplacement(String s, int k) {
+//         int n = s.length();
+//         int i = 0;
+//         int j = 0;
+//         Map<Character, Integer> map = new HashMap<>(); 
+//         int maxLen = 0;
+//         while(i<=j && j<n){
+//             // add the new character in substring
+//             char ch = s.charAt(j);
+//             map.put(ch, map.getOrDefault(ch,0)+1);
+
+//             // get the max freq in substring
+//             int maxFreq = 0;
+//             for(Character character: map.keySet()) 
+//                  maxFreq = Math.max(maxFreq, map.get(character));
+
+//             // if substring is not valid => shrink
+//             if(!((j-i+1 - maxFreq) <= k)){
+//                 map.put(s.charAt(i), map.get(s.charAt(i))-1);
+//                 i++;
+//             }
+
+//             // max lengeth of new substring
+//             maxLen = Math.max(maxLen, j-i+1);
+//             j++;
+//         }
+//         return maxLen;
+//     }
+// }
+
+
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int n = s.length();
+
+        int i = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        
+        int[] freq = new int[26]; 
+
+        for(int j=0; j<n; j++){
+            // add the new character in substring
+            char ch = s.charAt(j);
+            freq[ch - 'A']++;
+
+            // get the max freq in substring (Curr maxFreq v/s Curr Ele Count)
+            maxFreq = Math.max(maxFreq, freq[ch - 'A']);
+
+            // if substring is not valid => shrink
+            if(!((j-i+1 - maxFreq) <= k)){
+                freq[s.charAt(i) - 'A']--;
+                i++;
+            }
+
+            // max lengeth of new substring
+            maxLen = Math.max(maxLen, j-i+1);
+        }
+        return maxLen;
+    }
+}
+
